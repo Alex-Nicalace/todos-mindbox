9b048f155fa843614700f4e8096987fed122c8f2
Alex-Nicalace <anicalace@gmail.com>
2025-07-09 18:57:39 +0300

    chore: setup Storybook with initial config

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,50 @@ import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 // https://vite.dev/config/
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname =
+  typeof __dirname !== 'undefined'
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./vitest-setup.ts'],
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, '.storybook'),
+          }),
+        ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: 'playwright',
+            instances: [
+              {
+                browser: 'chromium',
+              },
+            ],
+          },
+          setupFiles: ['.storybook/vitest.setup.ts'],
+        },
+      },
+    ],
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // 'src' как '@'
+      '@': path.resolve(__dirname, 'src'),
       app: '/src/app',
       entities: '/src/entities',
       features: '/src/features',
