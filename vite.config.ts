940087dc3fd437e26c8891fb14da8b0e8b2e7d10
Alex-Nicalace <anicalace@gmail.com>
2025-07-09 14:03:44 +0300

    test: setup vitest and configure tsconfig, vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -5,6 +6,10 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   resolve: {
     alias: {
       '@': 'src',
