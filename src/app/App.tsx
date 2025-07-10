ab575add2181a1b04c1ceb46471d62368cd1ae42
Alex-Nicalace <anicalace@gmail.com>
2025-07-10 19:57:41 +0300

    feat(app): connect Providers and render TodosPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,16 @@
+import { Providers } from 'app/providers';
+import { TodosPage } from 'pages/todosPage';
+
 import './styles/index.scss';
 
 function App() {
-  return <div className="app light">app</div>;
+  return (
+    <Providers>
+      <div className="app light">
+        <TodosPage />
+      </div>
+    </Providers>
+  );
 }
 
 export default App;
