699f083426bd67219ead45e67d29b4d706aa90ae
Alex-Nicalace <anicalace@gmail.com>
2025-07-10 12:44:14 +0300

    docs(storybook): add stories for TodoInput

---
+++ src/features/TodoInput/ui/TodoInput.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { TodoInput } from './TodoInput';
+import { ThemeDecorator } from 'shared/config/storybook';
+
+const meta = {
+  title: 'features/TodoInput',
+  component: TodoInput,
+  parameters: {
+    layout: 'centered',
+    backgrounds: {},
+  },
+  tags: ['autodocs'],
+  args: {
+    name: 'TodoInput',
+    placeholder: 'What needs to be done?',
+  },
+} satisfies Meta<typeof TodoInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {};
+
+export const Dark: Story = {
+  decorators: [ThemeDecorator('dark')],
+};
