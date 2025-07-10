d3073c3b351b310384716e869354caee4f1b9cf6
Alex-Nicalace <anicalace@gmail.com>
2025-07-10 20:28:34 +0300

    feat(features/TodoInput): add logic to create todos on Enter

--- src/features/TodoInput/ui/TodoInput.tsx
+++ src/features/TodoInput/ui/TodoInput.tsx
@@ -1,19 +1,35 @@
-import type { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import { useState } from 'react';
 
+import { useTodos } from 'entities/todos';
 import ArrowIcon from 'shared/assets/icons/arrow-down.svg?react';
 import { cn } from 'shared/lib';
 
 import './TodoInput.scss';
 
-type TodoInputProps = DetailedHTMLProps<
-  InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement
->;
+type TodoInputProps = {
+  className?: string;
+};
 
-export function TodoInput({ className, ...props }: TodoInputProps) {
+export function TodoInput({ className }: TodoInputProps) {
+  const [todo, setTodo] = useState('');
+  const { dispatch } = useTodos();
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'Enter') {
+      dispatch({ type: 'ADD_TODO', payload: todo });
+      setTodo('');
+    }
+  }
+
   return (
     <div className={cn('todo-input', className)}>
-      <input className="todo-input__input" {...props} />
+      <input
+        className="todo-input__input"
+        placeholder="What needs to be done?"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
       <ArrowIcon className="todo-input__icon" width={24} height={24} />
     </div>
   );
