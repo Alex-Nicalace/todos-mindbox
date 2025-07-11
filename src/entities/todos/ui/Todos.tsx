2e38beb902b4f8052e64139676e6bc8b10913541
Alex-Nicalace <anicalace@gmail.com>
2025-07-11 13:17:08 +0300

    feat(entities/todos): style Todos list and add toggle todo logic

--- src/entities/todos/ui/Todos.tsx
+++ src/entities/todos/ui/Todos.tsx
@@ -1,9 +1,16 @@
 import { useMemo } from 'react';
+import { cn } from 'shared/lib';
 import { Checkbox } from 'shared/ui';
 import { useTodos } from '../context';
 
-export default function Todos() {
-  const { todosState } = useTodos();
+import './Todos.scss';
+
+export type TodosProps = {
+  className?: string;
+};
+
+export default function Todos({ className }: TodosProps) {
+  const { todosState, dispatch } = useTodos();
   const { todos, filter } = todosState;
   const filteredTodos = useMemo(
     () =>
@@ -13,13 +20,27 @@ export default function Todos() {
     [todos, filter]
   );
 
+  function handleToggleTodo(id: string) {
+    dispatch({ type: 'TOGGLE_TODO', payload: id });
+  }
+
   return (
-    <ul className="todos">
-      {filteredTodos.map((todo) => (
-        <li key={todo.id} className="todos__item">
-          <Checkbox checked={todo.completed} label={todo.title} />
+    <ul className={cn('todos', className)}>
+      {filteredTodos.length === 0 ? (
+        <li className="todos__item todos__item_empty">
+          📝 There are no tasks. Add the first one!
         </li>
-      ))}
+      ) : (
+        filteredTodos.map((todo) => (
+          <li key={todo.id} className="todos__item">
+            <Checkbox
+              checked={todo.completed}
+              label={todo.title}
+              onChange={() => handleToggleTodo(todo.id)}
+            />
+          </li>
+        ))
+      )}
     </ul>
   );
 }
